5ab79f6fc6f33692b083777c954206465f53dd31
Foos Mahamud <foosmahamud55@gmail.com>
2021-07-20 17:00:31 -0500

    updated Portforlio

---
+++ script.js
@@ -0,0 +1,23 @@
+// data types -> of variables
+var myName = "foos" // string
+var myAge = 32 // numbers
+var myDad = true //Booleans
+ //Array = -> Linked data -> indexed data
+ var myPets = ["key", "molly","black", "whole"]
+ //Objects -> Linked data -> Keys/value pairs
+
+
+ var myFamily = {
+     dad: "Mahamed Suudi",
+     mam: "Ambera Egal",
+     oldest: "Sister",
+     myParents: ["Mahamed Suudi", "Amera Igal"],
+     SayHello: function(name){
+            console.log("Hello" + name)
+     }
+
+}
+console.log("myfamily");
+console.log("Sayhello");
+console.log("myParents");
+console.log("oldest")
